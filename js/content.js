bb7c959b9a5470b6365189f7c1ee712964a1231d
Magnus Muru <magmur@ttu.ee>
2022-04-15 12:27:12 +0300

    Content page for Rain + Course pages

---
+++ js/content.js
@@ -0,0 +1,11 @@
+function showEmbedContent(elementId) {
+    let element = document.getElementById(elementId)
+
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    }
+    else {
+        element.style.display = "none";
+    }
+
+}
